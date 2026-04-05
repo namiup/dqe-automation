eff3133c37e8241ed7e6853097110a2604d854be
namiup <59592728+namiup@users.noreply.github.com>
2026-04-05 16:48:18 +0400

    Update conftest.py

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 import csv
 
 # Fixture to read the CSV file
-@pytest.fixture
+@pytest.fixture(scope="session")
 def read_file():
     file_path = "./PyTest Introduction/src/data/data.csv"
     csvfile = open(file_path, newline='')
@@ -11,6 +11,7 @@ def read_file():
     csvfile.close()
 
 # Fixture to validate the schema of the file
-
+@pytest.fixture(scope="session")
+def validate_schema(actual_schema, expected_schema):
 
 # Pytest hook to mark unmarked tests with a custom mark
