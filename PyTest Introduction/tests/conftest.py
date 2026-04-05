f7ebcd58b71c4bbaec7dccc1ddf6c815db6e5624
namiup <59592728+namiup@users.noreply.github.com>
2026-04-05 16:52:05 +0400

    Update conftest.py

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 import csv
 
+file_path = "./PyTest Introduction/src/data/data.csv"
+actual_schema = ["id", "name", "age", "email", "is_active"]
+
 # Fixture to read the CSV file
 @pytest.fixture(scope="session")
-def read_file():
-    file_path = "./PyTest Introduction/src/data/data.csv"
+def read_file(file_path):
     csvfile = open(file_path, newline='')
     reader = csv.DictReader(csvfile)
     yield reader
@@ -12,6 +14,8 @@ def read_file():
 
 # Fixture to validate the schema of the file
 @pytest.fixture(scope="session")
-def validate_schema(actual_schema, expected_schema):
+def validate_schema(actual_schema, read_file):
+    expected_schema = read_file.fieldnames
+    assert actual_schema == expected_schema, f"Schema mismatch: {actual_schema} != {expected_schema}"
 
 # Pytest hook to mark unmarked tests with a custom mark
