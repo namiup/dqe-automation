ded18a6b514c707f71c9d21869d58d6cc01d96fb
namiup <59592728+namiup@users.noreply.github.com>
2026-04-05 16:06:21 +0400

    Update conftest.py

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -2,7 +2,9 @@ import pytest
 import pandas as pd
 
 # Fixture to read the CSV file
-
+@pytest.fixture.get_file():
+  file_path = "./PyTest Introduction/src/data/data.csv"
+  return file_path
 
 # Fixture to validate the schema of the file
 
