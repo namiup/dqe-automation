a768c5a92b0a3e345e549594e609592f0b116751
namiup <59592728+namiup@users.noreply.github.com>
2026-04-05 17:00:44 +0400

    Update conftest.py

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -1,20 +1,21 @@
 import pytest
 import csv
 
-file_path = "./PyTest Introduction/src/data/data.csv"
-actual_schema = ["id", "name", "age", "email", "is_active"]
+file_path = ["./PyTest Introduction/src/data/data.csv"]
+actual_schema = [["id", "name", "age", "email", "is_active"]]
 
 # Fixture to read the CSV file
 @pytest.fixture(scope="session")
-def read_file(file_path):
-    csvfile = open(file_path, newline='')
+def read_file(params=file_path):
+    csvfile = open(request.param, newline='')
     reader = csv.DictReader(csvfile)
     yield reader
     csvfile.close()
 
 # Fixture to validate the schema of the file
 @pytest.fixture(scope="session")
-def validate_schema(actual_schema, read_file):
+def validate_schema(params=actual_schema, read_file):
+    actual_schema = request.param
     expected_schema = read_file.fieldnames
     assert actual_schema == expected_schema, f"Schema mismatch: {actual_schema} != {expected_schema}"
 
