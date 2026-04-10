2ec98f12238012ccae027c26d8e374e164a0bf08
namiup <59592728+namiup@users.noreply.github.com>
2026-04-10 08:43:22 +0400

    Update data_quality_validation_library.py

--- PyTest DQ Framework/src/data_quality/data_quality_validation_library.py
+++ PyTest DQ Framework/src/data_quality/data_quality_validation_library.py
@@ -31,5 +31,6 @@ class DataQualityLibrary:
 
     @staticmethod
     def check_not_null_values(df, column_names=None):
-        col for df.column_names:
-            col.not_null
+        pass
+        # col for df.column_names:
+        #     col.not_null
