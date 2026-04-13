ca4078bb10a54c32bab5ed1f9b87d52323d6aa61
namiup <59592728+namiup@users.noreply.github.com>
2026-04-13 19:51:05 +0400

    Update test_examples.py

--- PyTest DQ Framework/tests/test_examples.py
+++ PyTest DQ Framework/tests/test_examples.py
@@ -37,19 +37,3 @@ def target_data_factory(parquet_reader):
     def _factory(target_path, include_subfolders=True):
         return parquet_reader.process(target_path, include_subfolders=include_subfolders)
     return _factory
-
-@pytest.fixture(scope='module')
-def test_facility_data(target_data_factory):
-    path = '/parquet_data/facility_name_min_time_spent_per_visit_date'
-    target_data = target_data_factory(path)
-    return target_data
-
-
-@pytest.mark.parquet_data
-def test_check_dataset_is_not_empty(test_facility_data, data_quality_library):
-    data_quality_library.check_dataset_is_not_empty(test_facility_data)
-
-
-# @pytest.mark.parquet_data
-# def test_check_count(source_data, test_facility_data, data_quality_library):
-#     data_quality_library.check_count(source_data, test_facility_data)
