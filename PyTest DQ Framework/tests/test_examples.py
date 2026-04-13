a386b8369dec47b8c215daca9ed4dab5b92f3c27
namiup <59592728+namiup@users.noreply.github.com>
2026-04-13 17:08:55 +0400

    Update test_examples.py

--- PyTest DQ Framework/tests/test_examples.py
+++ PyTest DQ Framework/tests/test_examples.py
@@ -33,7 +33,8 @@ def source_data(db_connection):
 
 
 @pytest.fixture(scope='module')
-def target_data(parquet_reader, target_path):
+def target_data(parquet_reader):
+    target_path = '/parquet_data/facility_name_min_time_spent_per_visit_date'
     target_data = parquet_reader.process(target_path, include_subfolders=True)
     return target_data
 
