faadb95bfb0c46d27baeb8078685190a52f60955
namiup <59592728+namiup@users.noreply.github.com>
2026-04-13 11:54:55 +0400

    Update parquet_reader.py

---
+++ PyTest DQ Framework/src/connectors/file_system/parquet_reader.py
@@ -0,0 +1,9 @@
+@pytest.fixture(scope='session')
+def parquet_reader(request):
+    try:
+        reader = ParquetReader()
+        yield reader
+    except Exception as e:
+        pytest.fail(f"Failed to initialize ParquetReader: {e}")
+    finally:
+        del reader
