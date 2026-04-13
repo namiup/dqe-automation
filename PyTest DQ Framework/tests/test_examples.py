1b983747e9902f1c76fb054bafb399d8c9c555b4
namiup <59592728+namiup@users.noreply.github.com>
2026-04-13 19:44:06 +0400

    Update test_examples.py

--- PyTest DQ Framework/tests/test_examples.py
+++ PyTest DQ Framework/tests/test_examples.py
@@ -45,9 +45,9 @@ def test_facility_data(target_data_factory):
     return target_data
 
 
-# @pytest.mark.parquet_data
-# def test_check_dataset_is_not_empty(test_facility_data, data_quality_library):
-#     data_quality_library.check_dataset_is_not_empty(test_facility_data)
+@pytest.mark.parquet_data
+def test_check_dataset_is_not_empty(test_facility_data, data_quality_library):
+    data_quality_library.check_dataset_is_not_empty(test_facility_data)
 
 
 # @pytest.mark.parquet_data
