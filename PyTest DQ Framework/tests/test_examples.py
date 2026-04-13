d392df2adf6573312c44e567aabbf40b8931e322
namiup <59592728+namiup@users.noreply.github.com>
2026-04-13 19:57:04 +0400

    Update test_examples.py

--- PyTest DQ Framework/tests/test_examples.py
+++ PyTest DQ Framework/tests/test_examples.py
@@ -5,17 +5,7 @@ Author(s): Name Surname
 """
 
 import pytest
-from src.connectors.file_system.parquet_reader import ParquetReader
 
-@pytest.fixture(scope='session')
-def parquet_reader(request):
-    try:
-        reader = ParquetReader(None)
-        yield reader
-    except Exception as e:
-        pytest.fail(f"Failed to initialize ParquetReader: {e}")
-    finally:
-        del reader
 
 @pytest.mark.parquet_data
 def test_connect(db_connection):
@@ -32,8 +22,4 @@ def source_data(db_connection):
     return source_data
 
 
-@pytest.fixture(scope='module')
-def target_data_factory(parquet_reader):
-    def _factory(target_path, include_subfolders=True):
-        return parquet_reader.process(target_path, include_subfolders=include_subfolders)
-    return _factory
+
