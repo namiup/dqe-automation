d8445fe464cb933b24e4ef46b2ac99049993e2f2
namiup <59592728+namiup@users.noreply.github.com>
2026-04-13 17:06:11 +0400

    Update test_facility_name_min_time_spent_per_visit_date.py

--- PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
+++ PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
@@ -6,7 +6,10 @@ Author(s): Name Surname
 
 import pytest
 
+@pytest.fixture
+def target_path():
+    return '/parquet_data/facility_name_min_time_spent_per_visit_date'
+
 @pytest.mark.parquet_data
 def test_check_dataset_is_not_empty(target_data, data_quality_library):
-    target_path = '/parquet_data/facility_name_min_time_spent_per_visit_date'
     data_quality_library.check_dataset_is_not_empty(target_data(target_path))
