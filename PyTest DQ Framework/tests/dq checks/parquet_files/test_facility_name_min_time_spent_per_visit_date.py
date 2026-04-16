ac9289d476093a7e50aafacf23dd3b7f9d62eed5
namiup <59592728+namiup@users.noreply.github.com>
2026-04-16 09:46:55 +0400

    Update test_facility_name_min_time_spent_per_visit_date.py

--- PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
+++ PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
@@ -7,6 +7,38 @@ Author(s): Name Surname
 import pytest
 
 @pytest.fixture(scope='module')
+def source_data(db_connection):
+    source_query = """
+        SELECT
+        f.facility_name,
+        v.visit_timestamp::date AS visit_date,
+        MIN(v.duration_minutes) AS min_time_spent
+    FROM
+        visits v
+    JOIN facilities f 
+        ON f.id = v.facility_id
+    GROUP BY
+        f.facility_name,
+        visit_date
+    UNION ALL  -- misstake
+    SELECT
+        f.facility_name,
+        v.visit_timestamp::date AS visit_date,
+        MIN(v.duration_minutes) AS min_time_spent
+    FROM
+        visits v
+    JOIN facilities f 
+        ON f.id = v.facility_id
+    WHERE
+        f.facility_type = 'Clinic' 
+    GROUP BY
+        f.facility_name,
+        visit_date
+    """
+    source_data = db_connection.get_data_sql(source_query)
+    return source_data
+
+@pytest.fixture(scope='module')
 def test_facility_data(target_data_factory):
     path = '/parquet_data/facility_name_min_time_spent_per_visit_date'
     target_data = target_data_factory(path)
