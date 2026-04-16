ac43025cfe8fd5ba99db349132b12c1398ed80b0
namiup <59592728+namiup@users.noreply.github.com>
2026-04-16 09:46:39 +0400

    Update test_examples.py

--- PyTest DQ Framework/tests/test_examples.py
+++ PyTest DQ Framework/tests/test_examples.py
@@ -11,39 +11,3 @@ import pytest
 def test_connect(db_connection):
     df = db_connection.get_data_sql("select * from patients")
     assert len(df) == 30
-
-
-@pytest.fixture(scope='module')
-def source_data(db_connection):
-    source_query = """
-        SELECT
-        f.facility_name,
-        v.visit_timestamp::date AS visit_date,
-        MIN(v.duration_minutes) AS min_time_spent
-    FROM
-        visits v
-    JOIN facilities f 
-        ON f.id = v.facility_id
-    GROUP BY
-        f.facility_name,
-        visit_date
-    UNION ALL  -- misstake
-    SELECT
-        f.facility_name,
-        v.visit_timestamp::date AS visit_date,
-        MIN(v.duration_minutes) AS min_time_spent
-    FROM
-        visits v
-    JOIN facilities f 
-        ON f.id = v.facility_id
-    WHERE
-        f.facility_type = 'Clinic' 
-    GROUP BY
-        f.facility_name,
-        visit_date
-    """
-    source_data = db_connection.get_data_sql(source_query)
-    return source_data
-
-
-
